14d17a6862e62b7279d5d9fd12a390ef46ca4a8b
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-24 18:48:33 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Weight;
+            double Height;
+            double BMI;
+
+            Weight = double.Parse(textBox1.Text);
+            Height = double.Parse(textBox2.Text);
+
+            BMI = Weight / Math.Pow(Height, 2) ;
+
+            textBox3.Text = BMI.ToString();
+
+
+            if (BMI < 18.5)
+            {
+                textBox4.Text = "You are underweight";
+            }
+            else if (BMI >= 18.5 && BMI < 24.9)
+            {
+                textBox4.Text = "Your weight is normal";
+            }
+            else if (BMI < 24.9)
+            {
+                textBox4.Text = "You are overweight";
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
